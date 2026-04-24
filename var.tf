5bd4f0d43f23347a8796408fde9b65f62d36a58d
Ivanglish <ivanglish@Ivans-MacBook-Pro-83637.local>
2026-04-24 09:18:31 -0400

    changing subnets cidr

--- var.tf
+++ var.tf
@@ -17,15 +17,15 @@ variable "container_port" {
 variable "subnet_cidr_1" {
   type = map(string)
   default = {
-    test = "172.31.96.0/24"
-    prod = "172.31.98.0/24"
+    test = "10.0.1.0/24"
+    prod = "10.1.1.0/24"
   }
 }
 
 variable "subnet_cidr_2" {
   type = map(string)
   default = {
-    test = "172.31.97.0/24"
-    prod = "172.31.99.0/24"
+    test = "10.0.2.0/24"
+    prod = "10.1.2.0/24"
   }
 }
